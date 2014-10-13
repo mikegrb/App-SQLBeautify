840c23052940935e2b75c3a76864f08299e80afd
Michael Greb <michael@thegrebs.com>
2014-10-13 11:48:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+App::SQLBeautify - Blah blah blah
+
+# SYNOPSIS
+
+    use App::SQLBeautify;
+
+# DESCRIPTION
+
+App::SQLBeautify is
+
+# AUTHOR
+
+Mike Greb <michael@thegrebs.com>
+
+# COPYRIGHT
+
+Copyright 2014- Mike Greb
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/App/SQLBeautify.pm
@@ -0,0 +1,39 @@
+package App::SQLBeautify;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::SQLBeautify - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::SQLBeautify;
+
+=head1 DESCRIPTION
+
+App::SQLBeautify is
+
+=head1 AUTHOR
+
+Mike Greb E<lt>michael@thegrebs.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Mike Greb
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
